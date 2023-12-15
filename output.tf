12fd7ee4fec0e51227dbe80add24ee3b7735596d
vrkrishna-reddy <150343993+vrkrishna-reddy@users.noreply.github.com>
2023-12-15 15:26:36 +0530

    Create output.tf

---
+++ output.tf
@@ -0,0 +1,14 @@
+output "name" {
+    description = "Resource Group Id"
+    value = azurerm_resource_group.rg.name
+}
+
+output "location" {
+    description = "Resource Group Location"
+    value = azurerm_resource_group.rg.location
+}
+
+output "id" {
+    description = "Resource Group Id"
+    value = azurerm_resource_group.rg.id
+}
