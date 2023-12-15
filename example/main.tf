af5fd71a6438b834181530d8d588656730ee0027
mallik-raj <53124649+mallik-raj@users.noreply.github.com>
2023-12-15 16:11:40 +0530

    fasfdas
    dadas

--- example/main.tf
+++ example/main.tf
@@ -8,8 +8,3 @@ provider "azurerm" {
     client_secret = var.client_secret
 }
 
-module "resource-group" {
-  source  = "app.terraform.io/Sage_test/resource-group/azurerm"
-  version = "1.0.0"
-  resource_group_name = "app-service-rg"
-  location            = "EAST US 2"
